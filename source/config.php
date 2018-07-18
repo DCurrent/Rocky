5bbe512df34489530e1792084c5e9f1cc4931503
Caskey, Damon V <dc@caskeys.com>
2018-07-18 15:56:04 -0400

    Import 'config.php' file to replace 'settings.php'.

--- source/config.php
+++ source/config.php
@@ -3,21 +3,22 @@
 	abstract class APPLICATION_SETTINGS
 	{
 		const VERSION 		= '1.0.1';
-		const NAME			= 'Waukegan';
+		const NAME			= 'EHS Training';
 		const ADMINS		= 'dwhibb0, dvcask2';
 		const ADMIN_MAIN 	= 'dvcask2';
-		const AUTHENTICATE_URL = 'http://ehs.uky.edu/apps/waukegan';
+		const AUTHENTICATE_URL = 'https://ehs.uky.edu/apps/rocky';
+		const DIRECTORY_PRIME	= '/apps/rocky';
 		const TIME_FORMAT		= 'Y-m-d H:i:s';
 	}
 	
 	abstract class DATABASE
 	{
 		const 
-			HOST 		= 'gensql.ad.uky.edu\general',	// Database host (server name or address)
-			NAME 		= 'ehs_ticket',					// Database logical name.
-			USER 		= 'EHSInfo_User',				// User name to access database.
-			PASSWORD	= 'ehsinfo',					// Password to access database.
-			CHARSET		= 'UTF-8';						// Character set.
+			//HOST 		= 'gensql.ad.uky.edu\general',	// Database host (server name or address)
+			NAME 		= 'ehs_training';				// Database logical name.
+			//USER 		= 'EHSInfo_User',				// User name to access database.
+			//PASSWORD	= 'ehsinfo',					// Password to access database.
+			//CHARSET	= 'UTF-8';					// Character set.
 	}
 
 	abstract class MAILING
@@ -26,7 +27,7 @@
 			TO		= '',
 			CC		= '',
 			BCC		= 'dc@caskeys.com',
-			SUBJECT = 'Waukegan Ticket Alert',
+			SUBJECT = 'Training Alert',
 			FROM 	= 'ehs_noreply@uky.edu';
 	}	
 
